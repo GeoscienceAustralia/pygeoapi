bf3763366116533e08371de9c27e4bc858768b02
Sayaka Sandin <Sayaka.Sandin@ga.gov.au>
2024-02-27 11:08:23 +1100

    DAT-700 ###DEBUG 02

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -58,11 +58,12 @@ function error() {
 
 # Workdir
 cd ${PYGEOAPI_HOME}
-
+###DEBUG
+echo "this is line 62 in entrypoint.sh"
 echo "Trying to generate openapi.yml"
 pygeoapi openapi generate ${PYGEOAPI_CONFIG} --output-file ${PYGEOAPI_OPENAPI}
 ###DEBUG
-echo "this is line 65 in entrypoint.sh"
+echo "this is line 66 in entrypoint.sh"
 
 [[ $? -ne 0 ]] && error "openapi.yml could not be generated ERROR"
 
